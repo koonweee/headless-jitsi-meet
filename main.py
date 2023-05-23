c78e419ca8a460f7d0312c25bffb8da7897eeb23
Jeremy <jeremytkw98@gmail.com>
2023-05-23 13:56:57 +0800

    disable audio processing

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ args = sys.argv
 headless = True if 'headless' in args else False
 
 # URL parameters added with https://shawnchin.github.io/jitsi-url-generator/
-URL = 'https://meet.jit.si/MutualDecembersAverageCheaply#userInfo.displayName=%22pi%22&config.prejoinConfig.enabled=false&config.startWithVideoMuted=true&config.faceLandmarks.enableFaceCentering=false&config.disableTileEnlargement=true&config.notifications=[]'
+URL = 'https://meet.jit.si/MutualDecembersAverageCheaply#config.disableAP=true#userInfo.displayName=%22pi%22&config.prejoinConfig.enabled=false&config.startWithVideoMuted=true&config.faceLandmarks.enableFaceCentering=false&config.disableTileEnlargement=true&config.notifications=[]'
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('use-fake-ui-for-media-stream')
 chrome_options.add_argument('--disable-gpu')
 if headless:
+    print('headless')
     chrome_options.add_argument("--headless=new")
 
 # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
