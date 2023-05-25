8a51cc479a1fad458918c5e788cf1ec26939bba2
Jeremy <jeremytkw98@gmail.com>
2023-05-25 17:15:01 +0800

    Add simple webapp

--- server.py
+++ server.py
@@ -1,21 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from jitsi_client import join_call, leave_call
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    print('is this a console only statement?')
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
 @app.route('/start-call', methods=['POST'])
 def start_call():
     call_url = request.form['call_url']
     print('Incoming request to start call to url: ' + call_url)
     join_call(call_url)
-    return 'success'
+    return render_template('index.html')
 
 @app.route('/end-call', methods=['POST'])
 def end_call():
     leave_call()
-    return 'success'
+    return render_template('index.html')
