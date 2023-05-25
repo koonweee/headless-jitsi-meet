5428c83dbcd871a5b1c2148bee841f9e6fd1bb34
Jeremy <jeremytkw98@gmail.com>
2023-05-25 17:32:54 +0800

    add LED status code

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, render_template
 from jitsi_client import join_call, leave_call
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+GPIO.setup(12, GPIO.OUT) # LED connected to GPIO 12
 
 app = Flask(__name__)
 
@@ -11,10 +14,12 @@ def index():
 def start_call():
     call_url = request.form['call_url']
     print('Incoming request to start call to url: ' + call_url)
+    GPIO.output(12, GPIO.HIGH)
     join_call(call_url)
     return render_template('index.html')
 
 @app.route('/end-call', methods=['POST'])
 def end_call():
     leave_call()
+    GPIO.output(12, GPIO.LOW)
     return render_template('index.html')
